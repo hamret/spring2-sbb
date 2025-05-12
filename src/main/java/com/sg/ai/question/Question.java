314f138875050cbb5770628556ab05d94211af90
Daoll <daoll43000@gmail.com>
2025-05-12 13:47:21 +0900

    피니

--- src/main/java/com/sg/ai/question/Question.java
+++ src/main/java/com/sg/ai/question/Question.java
@@ -1,14 +1,23 @@
-package com.sg.ai;
+package com.sg.ai.question;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import com.sg.ai.answer.Answer;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
 
 
+@Getter
+@Setter
 @Entity
 public class Question {
 	
@@ -23,6 +32,9 @@ public class Question {
 	private String content;
 	
 	private LocalDateTime createDate;
+	
+	@OneToMany(mappedBy= "question", cascade = CascadeType.REMOVE)
+	private List<Answer> answerList;
 	
 
 }
